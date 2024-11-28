01b59bb8c86dbebf87b6a87395d6af9d9658d92b
Tengku Asyraf (RP) <tengku_asyraf@rp.edu.sg>
2024-11-28 14:04:13 +0800

    adds new components

--- src/components/App.js
+++ src/components/App.js
@@ -6,22 +6,35 @@ function App() {
   const [habits, setHabits] = useState([]);
 
   const handleAddHabit = (habitName) => {
-    // TODO: write code to add a new habit here
+    const newHabit = {
+      id: Date.now(),
+      name: habitName,
+      completed: false,
+    };
+    setHabits([...habits, newHabit]);
   };
 
   const handleToggleHabit = (id) => {
-    // TODO: write code to toggle a habit's status
+    setHabits((prevHabits) =>
+      prevHabits.map((habit) =>
+        habit.id === id ? { ...habit, completed: !habit.completed } : habit
+      )
+    );
   };
 
   const handleDeleteHabit = (id) => {
-    // TODO: write code to delete a habit
+    setHabits((prevHabits) => prevHabits.filter((habit) => habit.id !== id));
   };
 
   return (
     <div>
       <h1>Habit Tracker</h1>
-      {/*TODO: add a form to add a new habit*/}
-      {/*TODO: add a list of habits*/}
+      <AddHabitForm onAddHabit={handleAddHabit} />
+      <HabitList
+        habits={habits}
+        onToggleHabit={handleToggleHabit}
+        onDeleteHabit={handleDeleteHabit}
+      />
     </div>
   );
 }
